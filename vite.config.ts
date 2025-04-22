7cd354b33f71511ed0a740c5517c96015896b2fe
y-m0 <74723193+y-m0@users.noreply.github.com>
2025-04-22 22:25:33 +0100

    Changes to memory context

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -8,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 8080,
+    port: 3000,
     cors: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
